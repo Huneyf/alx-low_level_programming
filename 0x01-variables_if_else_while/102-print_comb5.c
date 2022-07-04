9a9f2011cbb49d77e877cf534bd76c10c99e048b
Huneyf <suleymanfantahun@gmail.com>
2022-07-05 02:18:55 +0300

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,17 @@
  */
 int main(void)
 {
-        int digit1, digit2;
-        for (digit1 = 48; digit1 <= 57; digit1++)
-        {
-                for (digit2 = 49; digit2 <= 57; digit2++)
-                {
-			putchar(digit1);
-			putchar(digit2);
-			if (digit1 != 57 || n != 57)
-                        putchar(44);
-                        putchar(32);
-		}
-        }
+int digit1, digit2;
+for (digit1 = 48; digit1 <= 57; digit1++)
+{
+for (digit2 = 49; digit2 <= 57; digit2++)
+{
+putchar(digit1);
+putchar(digit2);
+if(digit1 != 57 || n != 57)
+putchar(44);
+putchar(32);
+}
+}
 return (0);
 }
-
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,8 +10,8 @@ int main(void)
 char alphabet;
 for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
-        putchar(alphabet);
+putchar(alphabet);
 }
 putchar('\n');
-    return (0);
+return (0);
 }
