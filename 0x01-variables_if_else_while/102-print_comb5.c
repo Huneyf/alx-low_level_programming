e0f804f7a29f8ef3b342d33c14d8735f5f47e7e2
Huneyf <suleymanfantahun@gmail.com>
2022-07-05 01:40:05 +0300

    if else scripts

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program that prints all possible different combinations of two two digits
+ * in ascending order separated by ,, followed by a space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int digit1, digit2;
+        for (digit1 = 48; digit1 <= 57; digit1++)
+        {
+                for (digit2 = 49; digit2 <= 57; digit2++)
+                {
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 != 57 || n != 57)
+                        putchar(44);
+                        putchar(32);
+		}
+        }
+return (0);
+}
+
