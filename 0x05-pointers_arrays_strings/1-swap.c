7444d4b51f04da402d9f17420793a0383698e3a4
Huneyf <suleymanfantahun@gmail.com>
2022-07-12 23:11:42 +0300

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,13 @@
-
+#include "main.h"
+/**
+ *swap_int-is a function to swap the value of two values
+ *@a: parameter 1
+ *@b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+int n;
+n = *a;
+*a = *b;
+*a = n;
+}
