7ceff982ed8ccc9683e84c25ee29cbe1f628bcf8
Huneyf <suleymanfantahun@gmail.com>
2022-07-15 23:03:55 +0300

    corrected

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
 * _strcat -> this is a funcion strcat
 * @dest: first param
@@ -7,14 +9,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int len = 0, i;
-while (dest[len])
-len++;
-for (i = 0; src[i] != 0; i++)
-{
-dest[len] = src[i];
-len += 1;
-}
-dest[len] = '\0';
-return (dest);
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
