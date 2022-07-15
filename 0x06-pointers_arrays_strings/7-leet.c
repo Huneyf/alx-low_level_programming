f2a7b24868dcad2e0a676ad850255a83c4b6b190
Huneyf <suleymanfantahun@gmail.com>
2022-07-15 22:36:54 +0300

    mandatory

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *leet - a fuction that changes leet to numbers
- *@x: parameter1
- *Return: sting
+ * leet - a fuction that changes leet to numbers
+ * @x: parameter1
+ * Return: string
  */
 char *leet(char *x)
 {
 int l = 5, a = 0, b;
-char tbr [5] = {'A', 'E', 'O', 'T', 'L'};
-char tbrw [5] = {'4', '3', '0', '7', '1'};
+char tr [5] = {'A', 'E', 'O', 'T', 'L'};
+char trw [5] = {'4', '3', '0', '7', '1'};
 while (x[a])
 {
 b = 0;
 while (b < l)
 {
-if (x[a] == tbr[b] || x[a] - 32 == tbr[b])
-x[a] = tbrw[b];
+if (x[a] == tr[b] || x[a] - 32 == tr[b])
+x[a] = trw[b];
 a++;
 }
 b++;
