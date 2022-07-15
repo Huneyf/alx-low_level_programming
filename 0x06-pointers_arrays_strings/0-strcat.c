e24333eda751b4b9a783fc841f18cbe0792ee178
Huneyf <suleymanfantahun@gmail.com>
2022-07-15 22:59:03 +0300

    corrected

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * _strcat - is a function that strcats second string to first string
- * @dest: parameter1
- * @src: parameter2
- * return: a string
- */
+* _strcat -> this is a funcion strcat
+* @dest: first param
+* @src: second param
+* Return: a string
+*/
 char *_strcat(char *dest, char *src)
 {
-int len = 0;
-int i;
+int len = 0, i;
 while (dest[len])
 len++;
 for (i = 0; src[i] != 0; i++)
 {
 dest[len] = src[i];
-i += 1;
+len += 1;
 }
 dest[len] = '\0';
 return (dest);
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * string_toupper - a function that changes lower case to upper case
- * @x: parameter1
- * Return: string
- */
+* string_toupper -> converting any string to upper case
+* @x: string param
+* Return: string
+*/
 char *string_toupper(char *x)
 {
-int i = 0;
-while (x[i])
-{
-if (s[i] >= 97 && x[i] <= 122)
-x[i] = x[i] - 32;
-i++;
-}
-return (x);
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
 }
