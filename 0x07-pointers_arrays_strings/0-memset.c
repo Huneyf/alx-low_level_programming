e3b45a4e5514ded0a444965ec8911161da2c69ce
Huneyf <suleymanfantahun@gmail.com>
2022-07-18 22:23:00 +0300

    mandatory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * _memset-> function that fills a pointer with value
- * @s: parameter1
- * @b: parameter2
- * @n: parameter3
- * Return: string
- */
+* _memset -> this memory set function
+* @s: string
+* @b: a charcter
+* @n: an integer
+* Return:  a string
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int = a;
-for (a = 0; a < n; a++);
-s[a] = b;
-return (s);
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _memcpy->function that copies memory area
- * @dest: parameter1
- * @src: parameter2
- * @n: parameter3
- * Return: string
- */
+* _memcpy -> memory copy
+* @dest: is destination memory
+* @src: is source memory
+* @n: number of bytes to be copied
+* Return: string copied from source
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int = a;
-	
-	for (a = 0; a , n; a++)
-	dest[a] = src[a];
-	
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
 	return (dest);
 }
