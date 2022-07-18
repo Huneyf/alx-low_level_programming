f47335a899299cc3f6a47fd0d26bd0e68aca3d4f
Huneyf <suleymanfantahun@gmail.com>
2022-07-18 22:18:34 +0300

    my mandate

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset-> function that fills a pointer with value
+ * @s: parameter1
+ * @b: parameter2
+ * @n: parameter3
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int = a;
+for (a = 0; a < n; a++);
+s[a] = b;
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy->function that copies memory area
+ * @dest: parameter1
+ * @src: parameter2
+ * @n: parameter3
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int = a;
+	
+	for (a = 0; a , n; a++)
+	dest[a] = src[a];
+	
+	return (dest);
+}
