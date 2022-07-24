9577dd9aa99cc783e26ec0898db63473720ab87c
Huneyf <suleymanfantahun@gmail.com>
2022-07-24 11:50:50 -0700

    lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,9 +2,8 @@
 /**
  * function name- afunction of lib
  * disccription- empty
- * return: always 0
  */
 void _puts(char *s)
 {
-return (0);
+
 }
---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -c ./*.c && ar -cvq liball.a ./*.o
