b08d47ef5bc8adc2b164797499222a4661d8cb7e
Huneyf <suleymanfantahun@gmail.com>
2022-08-05 22:31:01 +0300

    task 1 variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,21 +5,20 @@
 /**
  * sum_them_all- is a function that adds up any number of functions.
  * @n: any Number of intigers
- * Returns: Always 0
+ * Returns: sum of all numbers
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	va_start (ap, n);
-	int i, sum = 0;
-	if (n == 0)
+	unsigned int i, sum = 0;
+	if (n != 0)
 	{
-		return 0;
-	}
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + va_arg (ap, int);
+		sum = sum + va_arg (ap, unsigned int);
+	}
 	}
 	va_end (ap);
 	return (sum);
